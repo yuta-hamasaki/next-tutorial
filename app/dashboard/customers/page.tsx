af0cfe9fb9b99b3df90e3d66aee1f13c15bdc6b7
yuta-hamasaki <yh02052002@gmail.com>
2024-06-07 16:58:51 -0700

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <p>Customers page</p>
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      <p>Invoices page</p>
+    </div>
+  )
+}
+
+export default page
